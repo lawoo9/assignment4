a3c0464b8f3e3a404d850519712bd52815a5bf51
lawrence <lawoj91@hotmail.com>
2023-07-12 19:09:10 -0400

    assignment4

---
+++ datatype.py
@@ -0,0 +1,16 @@
+num1 = 5
+print(num1, 'is of type', type(num1))
+num2 = 2.0
+print(num2, 'is of type', type(num2))
+num3 = 1+2j
+print(num3, 'is of type', type(num3))
+name = 'python'
+print(name)
+message = 'python for beginners'
+print(message)
+# create a set named student_id
+student_id = {112, 114, 116, 118, 115}
+# display student_id elements
+print(student_id)
+# display type of student_id
+print(type(student_id))
---
+++ variable.py
@@ -0,0 +1,6 @@
+first_name =  "lawrence"
+middle_name = "ojomah"
+last_name =   "ojomah"
+full_name = first_name + "" + middle_name + "" + last_name
+print(full_name)
+
